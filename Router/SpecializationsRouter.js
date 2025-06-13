70bf6c151729eb5d25b77b3ca7945014f805fa2c
rudnooranil <rudnooranil@gmail.com>
2025-06-13 21:57:59 +0530

    changes

--- Router/SpecializationsRouter.js
+++ Router/SpecializationsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const genderController = require('../Controller/SpecializationsController'); 
-
-
-router.post('/createSpecializations', genderController.createSpecializations);
-router.get('/getSpecializations', genderController.getSpecializations);
-router.put('/updateSpecializations', genderController.updateSpecializations);
+const _Controller = require('../Controller/SpecializationsController'); 
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+router.post('/createSpecializations', upload.single('image'), _Controller.createSpecializations);
+router.get('/getSpecializations', _Controller.getSpecializations);
+router.put('/updateSpecializations', _Controller.updateSpecializations);
 
 module.exports = router;
